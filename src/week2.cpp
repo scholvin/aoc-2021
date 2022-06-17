7a5d047d25809463b12434c3a5ea73dd63a78a02
John Scholvin <john@scholvin.com>
2022-06-17 13:03:36 -0500

    day 9

--- src/week2.cpp
+++ src/week2.cpp
@@ -3,7 +3,9 @@
 
 #include <string>
 #include <vector>
-//#include <iostream>
+#include <unordered_set>
+#include <set>
+#include <iostream>
 
 namespace week2
 {
@@ -107,5 +109,114 @@ namespace week2
         }
 
         return sum;
+    }
+
+    long day09(char part)
+    {
+#if 0
+        const int WIDTH = 10;
+        const int HEIGHT = 5;
+        const std::string FILENAME = "../data/day09-smol.dat";
+#else
+        const int WIDTH = 100;
+        const int HEIGHT = 100;
+        const std::string FILENAME = "../data/day09.dat";
+#endif
+        typedef std::pair<uint8_t, uint8_t> coordinate_t;
+        std::array<std::array<uint8_t, HEIGHT>, WIDTH> seafloor;
+        std::vector<coordinate_t> lows;
+
+        std::ifstream infile(FILENAME);
+        std::string line;
+        int x = 0, y = 0;
+        while (std::getline(infile, line))
+        {
+            for (x = 0; x < WIDTH; x++)
+            {
+                seafloor[x][y] = line[x] - '0';
+            }
+            y++;
+        }
+
+        long result = 0;
+        for (y = 0; y < HEIGHT; y++)
+        {
+            for (x = 0; x < WIDTH; x++)
+            {
+                uint8_t left = (x == 0 ? 9 : seafloor[x-1][y]);
+                uint8_t right = (x == WIDTH-1 ? 9 : seafloor[x+1][y]);
+                uint8_t top = (y == 0 ? 9 : seafloor[x][y-1]);
+                uint8_t bottom = (y == HEIGHT-1 ? 9 : seafloor[x][y+1]);
+                if (seafloor[x][y] < left && seafloor[x][y] < right && seafloor[x][y] < top && seafloor[x][y] < bottom)
+                {
+                    if (part == 'a')
+                        result += seafloor[x][y] + 1;
+                    else
+                        lows.push_back(std::make_pair(x, y));
+                }
+            }
+        }
+
+        if (part == 'b')
+        {
+            // find the "basin" around each low, keep the three biggest ones
+            struct coordinate_hash
+            {
+                std::size_t operator() (const coordinate_t &coordinate) const {
+                    return std::hash<uint8_t>()(coordinate.first) ^ std::hash<uint8_t>()(coordinate.second);
+                }
+            };
+            typedef std::unordered_set<coordinate_t, coordinate_hash> basin_t;
+
+            std::multiset<long> areas;
+            for (auto l: lows)
+            {
+                basin_t basin;
+                basin.insert(l);
+                bool done = false;
+                // this is a little bit gross, something like O(b^2) in the size of the basins, but they shouldn't get that big
+                while (!done)
+                {
+                    basin_t work;
+                    // for every element in the basin, check all 4 neighbors, and if they're not a 9, and not already in the basin, add them
+                    for (auto c: basin)
+                    {
+                        uint8_t x = c.first;
+                        uint8_t y = c.second;
+                        // check left
+                        if (x > 0 && seafloor[x-1][y] != 9 && basin.find({x-1, y}) == basin.end())
+                            work.insert({x-1, y});
+                        // right
+                        if (x < WIDTH-1 && seafloor[x+1][y] != 9 && basin.find({x+1, y}) == basin.end())
+                            work.insert({x+1, y});
+                        // top
+                        if (y > 0 && seafloor[x][y-1] != 9 && basin.find({x, y-1}) == basin.end())
+                            work.insert({x, y-1});
+                        // bottom
+                        if (y < HEIGHT-1 && seafloor[x][y+1] != 9 && basin.find({x, y+1}) == basin.end())
+                            work.insert({x, y+1});
+                    }
+
+                    if (work.size() == 0)
+                    {
+                        done = true;
+                    }
+                    else
+                    {
+                        basin.merge(work);
+                    }
+                }
+                areas.insert(basin.size());
+            }
+            // return product of 3 biggest areas
+            result = 1;
+            auto it = areas.rbegin();
+            result *= *it++;
+            result *= *it++;
+            result *= *it;
+
+        }
+
+        return result;
     }
 };
--- src/week2.h
+++ src/week2.h
@@ -6,10 +6,14 @@ namespace week2
 {
     long day08a();
     long day08b();
+    long day09(char part);
 
     const runner::method_map_t method_map =
     {
         { "8a", &day08a },
         { "8b", &day08b },
+        { "9a", std::bind(&day09, 'a') },
+        { "9b", std::bind(&day09, 'b') },
+
     };
 };
