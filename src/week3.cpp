fa51d75c875f37a0503a7962563ef9d903794ab7
John Scholvin <john@scholvin.com>
2022-06-20 22:41:00 -0500

    day15 done and correct, but very slow (Dijkstra)

--- src/week3.cpp
+++ src/week3.cpp
@@ -13,23 +13,30 @@
 
 namespace week3
 {
-    long day15()
+    struct coordinate_t
     {
-#if 0
-        const int SIZE = 10;
-        const std::string FILENAME = "../data/day15-smol.dat";
-        const int SANE = 6;
-#else
-        const int SIZE = 100;
-        const std::string FILENAME = "../data/day15.dat";
-        const int SANE = 7;
-#endif
+        int x, y;
+        bool operator==(const coordinate_t& other) const { return x == other.x && y == other.y; }
+    };
 
-        std::array<std::array<uint8_t, SIZE>, SIZE> grid;
-        readers::read_dense_2d_matrix(FILENAME, readers::digit_parser(), grid);
+    struct coordinate_hash {
+        std::size_t operator() (const coordinate_t &coordinate) const
+        {
+            return std::hash<int>()(coordinate.x) ^ std::hash<int>()(coordinate.y);
+        }
+    };
 
-        /* The grid we just read represents the edge weight in to that node.
-           For example:
+    struct vertex_prop_t { coordinate_t coordinates; };
+    struct edge_prop_t { int weight; };
+    using graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, vertex_prop_t, edge_prop_t>;
+    using vertex_t = boost::graph_traits<graph_t>::vertex_descriptor;
+    typedef std::unordered_map<coordinate_t, vertex_t, coordinate_hash> vertex_map_t;
+
+    template <int SIZE>
+    void populate_graph(const std::array<std::array<uint8_t, SIZE>, SIZE>& grid, graph_t& graph, vertex_map_t& vmap)
+    {
+         /* The grid represents the edge weight *into* that node.
+            For example:
 
                 456
                 789
@@ -37,32 +44,10 @@ namespace week3
             In this graph, the edge from [0][1] to [0][0] has weight 4, and the edge from [0][0] to [0][1] is 5.
 
             So now we go through the usual rigamarole of populating a Boost graph based on this. Vertex
-            identifiers will be a tuple of the {x,y} location.
+            identifiers will be a tuple of the {x,y} location (coordinate_t).
         */
 
-        struct coordinate_t
-        {
-            int x, y;
-            bool operator==(const coordinate_t& other) const { return x == other.x && y == other.y; }
-        };
-
-        struct coordinate_hash {
-            std::size_t operator() (const coordinate_t &coordinate) const
-            {
-                return std::hash<int>()(coordinate.x) ^ std::hash<int>()(coordinate.y);
-            }
-        };
-
-        struct vertex_prop_t { coordinate_t coordinates; };
-        struct edge_prop_t { int weight; };
-        using graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, vertex_prop_t, edge_prop_t>;
-        using vertex_t = boost::graph_traits<graph_t>::vertex_descriptor;
-        // using edge_t = boost::graph_traits<graph_t>::edge_descriptor;
-
-        graph_t graph;
-
         // the usual stupid mapping problem - start with creating/inserting all the vertices
-        std::unordered_map<coordinate_t, vertex_t, coordinate_hash> vmap;
         for (int x = 0; x < SIZE; x++)
         {
             for (int y = 0; y < SIZE; y++)
@@ -95,10 +80,92 @@ namespace week3
                 }
             }
         }
+    }
+
+    long day15a()
+    {
+#if 0
+        const int SIZE = 10;
+        const std::string FILENAME = "../data/day15-smol.dat";
+        const int SANE = 6;
+#else
+        const int SIZE = 100;
+        const std::string FILENAME = "../data/day15.dat";
+        const int SANE = 7;
+#endif
+
+        std::array<std::array<uint8_t, SIZE>, SIZE> grid;
+        readers::read_dense_2d_matrix(FILENAME, readers::digit_parser(), grid);
+
+        graph_t graph;
+        vertex_map_t vmap;
+        populate_graph<SIZE>(grid, graph, vmap);
 
         // sanity check
         auto x = boost::edge(vmap[{1,0}], vmap[{2,0}], graph);
         assert(x.second && graph[x.first].weight == SANE);
+
+        std::vector<int> distances(boost::num_vertices(graph));
+        vertex_t start = vmap[{0, 0}];
+        boost::dijkstra_shortest_paths(graph, start,
+                                       boost::weight_map(boost::get(&edge_prop_t::weight, graph))
+                                            .distance_map(boost::make_iterator_property_map(distances.begin(),
+                                                          boost::get(boost::vertex_index, graph))));
+        vertex_t finish = vmap[{SIZE-1,SIZE-1}];
+        return distances[finish];
+    }
+
+    long day15b()
+    {
+        const int MULT = 5;
+#if 0
+        const int GSIZE = 10;
+        const std::string FILENAME = "../data/day15-smol.dat";
+        const int SANE = 6;
+#else
+        const int GSIZE = 100;
+        const std::string FILENAME = "../data/day15.dat";
+        const int SANE = 7;
+#endif
+         const int SIZE = GSIZE * MULT;
+
+        std::array<std::array<uint8_t, GSIZE>, GSIZE> little_grid;
+        readers::read_dense_2d_matrix(FILENAME, readers::digit_parser(), little_grid);
+
+        std::array<std::array<uint8_t, SIZE>, SIZE> grid { 0 };
+
+        // i,j is the big grid (5x5) of the smaller (GSIZE x GSIZE) grids
+        // copy them, increasing values as we go right and down (weird mod 9)
+        for (int i = 0; i < MULT; i++)
+        {
+            for (int j = 0; j < MULT; j++)
+            {
+                for (int x = 0; x < GSIZE; x++)
+                {
+                    for (int y = 0; y < GSIZE; y++)
+                    {
+                        grid[x + i*GSIZE][y + j*GSIZE] = little_grid[x][y] + i + j;
+                        if (grid[x + i*GSIZE][y + j*GSIZE] > 9)
+                        {
+                            grid[x + i*GSIZE][y + j*GSIZE] -= 9;
+                        }
+                    }
+                }
+            }
+        }
+#if 0
+        for (int y = 0; y < SIZE; y++)
+        {
+            for (int x = 0; x < SIZE; x++)
+            {
+                std::cout << static_cast<int>(grid[x][y]);
+            }
+            std::cout << std::endl;
+        }
+#endif
+        graph_t graph;
+        vertex_map_t vmap;
+        populate_graph<SIZE>(grid, graph, vmap);
 
         std::vector<int> distances(boost::num_vertices(graph));
         vertex_t start = vmap[{0, 0}];
--- src/week3.h
+++ src/week3.h
@@ -4,10 +4,12 @@
 
 namespace week3
 {
-    long day15();
+    long day15a();
+    long day15b();
 
     const runner::method_map_t method_map =
     {
-        { "15a", &day15 },
+        { "15a", &day15a },
+        { "15b", &day15b },
     };
 };
